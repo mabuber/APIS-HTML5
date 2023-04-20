aec1db8b3bb7f990ccedf22b89cb85665d84707a
mabuber <123788735+mabuber@users.noreply.github.com>
2023-04-21 01:04:35 +0200

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,19 @@
+const fileAPIsSupported = window.File && window.FileReader && window.FileList;
+
+if (fileAPIsSupported) {
+  alert("API's verificadas");
+} else {
+  alert("API File no soportada");
+}
+
+document.getElementById('videoFile').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function(event) {
+    video.src = event.target.result;
+    alert('Cargando...');
+  };
+  reader.readAsDataURL(file);
+
+}, false);
